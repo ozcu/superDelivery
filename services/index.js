86268bc6cc16e16c92dfea51e524d0cfce7bcd19
Ugurhan Buyukozcu <ugurhan.buyukozcu@gmail.com>
2021-10-05 01:04:23 +0300

    Organize codebase

--- services/index.js
+++ services/index.js
@@ -2,5 +2,12 @@ const userService = require('./user-service')
 const productService = require('./product-service')
 const orderService = require('./order-service')
 const courierService = require('./courier-service')
+const basketService = require('./basket-service')
 
-module.exports = { userService, productService, orderService, courierService }
+module.exports = {
+  userService,
+  productService,
+  orderService,
+  courierService,
+  basketService,
+}
