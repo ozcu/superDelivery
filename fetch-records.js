0c2b32e7f15afbf2c383810c25ed0528f89120b6
Ugurhan Buyukozcu <ugurhan.buyukozcu@gmail.com>
2021-10-05 00:59:55 +0300

    Remove unrelevant dependencies

--- fetch-records.js
+++ fetch-records.js
@@ -1,15 +1,10 @@
 require('./mongo-connection')
-const User = require('./models/user')
-const Product = require('./models/product')
-const Basket = require('./models/basket')
-const Order = require('./models/order')
 
 const {
   userService,
   productService,
   courierService,
 } = require('./services/index')
-const courier = require('./models/courier')
 
 async function main() {
   const couriers = await courierService.load()
