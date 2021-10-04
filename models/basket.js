f5fa8b07b67c8d6b2ac8b78c258faf02ce17e27f
Ugurhan Buyukozcu <ugurhan.buyukozcu@gmail.com>
2021-10-05 01:01:19 +0300

    Move methods from model to service

--- models/basket.js
+++ models/basket.js
@@ -2,47 +2,10 @@ const mongoose = require('mongoose')
 
 const BasketSchema = new mongoose.Schema(
   {
-    product: [],
+    products: [],
     basketTotal: Number,
   },
   { timestamps: true },
 )
 //BasketSchema.plugin(require('mongoose-autopopulate'))
 module.exports = mongoose.model('Basket', BasketSchema)
-
-/*
-
-class Basket{
-    constructor(product, basketTotal){
-        this.product = []
-        this.basketTotal = basketTotal
-        
-    }
-    
-    addToBasketTotal (price){
-        return new Promise((resolve,reject) => {
-        this.basketTotal =  this.basketTotal + price,(err) =>{
-        if (err) return reject(err)
-        resolve(this.basketTotal)
-        }
-        })
-    }
-
-
-    removeFromBasketTotal (price){
-        return new Promise((resolve,reject) => {
-        this.basketTotal =  this.basketTotal - price,(err) =>{
-        if (err) return reject(err)
-        resolve(this.basketTotal)
-        }
-        })
-    }
-    
-    
-}
-
-
-
-module.exports = Basket
-
-*/
