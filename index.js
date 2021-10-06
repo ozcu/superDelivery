8ef95d766efee71ab6426dfca8d6152af4ad2cb3
Ugurhan Buyukozcu <ugurhan.buyukozcu@gmail.com>
2021-10-06 23:52:37 +0300

    Add courier and basket routers

--- index.js
+++ index.js
@@ -14,11 +14,15 @@ const indexRouter = require('./routes/index')
 const usersRouter = require('./routes/users')
 const productsRouter = require('./routes/products')
 const depotsRouter = require('./routes/depots')
+const couriersRouter = require('./routes/couriers')
+const basketsRouter = require('./routes/baskets')
 
 app.use('/', indexRouter)
 app.use('/users', usersRouter)
 app.use('/products', productsRouter)
 app.use('/depots', depotsRouter)
+app.use('/couriers', couriersRouter)
+app.use('/baskets', basketsRouter)
 
 app.listen(3000, () => {
   console.log('started listening')
