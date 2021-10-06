6082c9b4bea548a6bcee550dc6116bba4f96c599
Ugurhan Buyukozcu <ugurhan.buyukozcu@gmail.com>
2021-10-06 18:50:26 +0300

    Change endpoint to local db due connection errors

--- mongo-connection.js
+++ mongo-connection.js
@@ -5,7 +5,7 @@ const dbURI2 = 'mongodb://localhost/GetirClone'
 //connect to mongodb
 const dbURI =
   'mongodb+srv://Admin:admin1234@cluster0.tw5yi.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
-mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(dbURI2, { useNewUrlParser: true, useUnifiedTopology: true })
 
 var db = mongoose.connection
 db.on('error', console.error.bind(console, 'connection error:'))
