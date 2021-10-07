e95c33fae0bc7921f1b24071712959af19d00e84
Ugurhan Buyukozcu <ugurhan.buyukozcu@gmail.com>
2021-10-07 16:18:46 +0300

    Fix findbyId method issue

--- services/basket-service.js
+++ services/basket-service.js
@@ -4,20 +4,18 @@ const productService = require('./product-service')
 
 class BasketService extends BaseService {
   async addProductToBasket(userId, productId) {
-    const [newUser] = await userService.findById(userId)
-    const [newProduct] = await productService.findById(productId)
-
+    const newUser = await userService.find(userId)
+    const newProduct = await productService.find(productId)
     await newUser.basket.products.push(newProduct)
     newUser.basket.basketTotal += newProduct.price
-
     await userService.update(userId, newUser)
 
     return newUser, newProduct
   }
 
   async removeProductFromBasket(userId, productId) {
-    const [newUser] = await userService.findById(userId)
-    const [newProduct] = await productService.findById(productId)
+    const newUser = await userService.find(userId)
+    const newProduct = await productService.find(productId)
 
     const index = await newUser.basket.products.indexOf(newProduct)
     await newUser.basket.products.splice(index, 1)
