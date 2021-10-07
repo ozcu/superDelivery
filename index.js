075d3fc372adaee5645c6df3baad7fc020440ac4
Ugurhan Buyukozcu <ugurhan.buyukozcu@gmail.com>
2021-10-07 16:21:26 +0300

    Add courier-booking & order service handling

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 require('./mongo-connection')
 
 const express = require('express')
-//const bodyParser = require('body-parser') //deprecated express içine dahil oldu sanırım bir test edip kaldır
 
 const app = express()
 
@@ -16,6 +15,8 @@ const productsRouter = require('./routes/products')
 const depotsRouter = require('./routes/depots')
 const couriersRouter = require('./routes/couriers')
 const basketsRouter = require('./routes/baskets')
+const courierBookingRouter = require('./routes/courier-booking')
+const ordersRouter = require('./routes/orders')
 
 app.use('/', indexRouter)
 app.use('/users', usersRouter)
@@ -23,6 +24,8 @@ app.use('/products', productsRouter)
 app.use('/depots', depotsRouter)
 app.use('/couriers', couriersRouter)
 app.use('/baskets', basketsRouter)
+app.use('/courier-booking', courierBookingRouter)
+app.use('/orders', ordersRouter)
 
 app.listen(3000, () => {
   console.log('started listening')
