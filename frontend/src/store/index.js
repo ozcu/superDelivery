5afc0bcd039559f01c082ba6279a38e5a42f007f
Ugurhan Buyukozcu <ugurhan.buyukozcu@gmail.com>
2021-10-18 00:16:21 +0300

    global user and modal states added

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -3,11 +3,25 @@ import Vuex from 'vuex'
 
 Vue.use(Vuex)
 
+const Mutations = {
+    TOGGLEMODAL: 'TOGGLEMODAL',
+}
+
 export default new Vuex.Store({
     state: {
+        showModal: false,
         user: null,
     },
-    mutations: {},
-    actions: {},
+    mutations: {
+        [Mutations.TOGGLEMODAL](state) {
+            state.showModal = !state.showModal
+        },
+    },
+    actions: {
+        toggleModal({ commit }) {
+            commit.apply(Mutations.TOGGLEMODAL)
+        },
+    },
+
     modules: {},
 })
