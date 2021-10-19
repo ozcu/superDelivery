171eb16abfaf0ec9a85fffc746a39ef254173c0f
Ugurhan Buyukozcu <ugurhan.buyukozcu@gmail.com>
2021-10-20 00:10:40 +0300

    add backend directory

---
+++ backend/services/index.js
@@ -0,0 +1,17 @@
+const userService = require('./user-service')
+const productService = require('./product-service')
+const orderService = require('./order-service')
+const depotService = require('./depot-service')
+const basketService = require('./basket-service')
+const courierService = require('./courier-service')
+courierBookingService = require('./courier-booking-service')
+
+module.exports = {
+  userService,
+  productService,
+  orderService,
+  depotService,
+  basketService,
+  courierService,
+  courierBookingService,
+}
