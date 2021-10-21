a6718ac708848a2d513de03281f80b4f42f286da
Ugurhan Buyukozcu <ugurhan.buyukozcu@gmail.com>
2021-10-21 23:38:47 +0300

    jwt cookie and route guard added

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,8 +1,11 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../components//Home.vue'
-import Login from '../components//Login.vue'
+import VueCookies from 'vue-cookies'
+
+import Home from '../components/Home.vue'
+import Login from '../components/Login.vue'
 import SignupForm from '../components/SignupForm.vue'
+import Products from '../components/Products.vue'
 
 Vue.use(VueRouter)
 
@@ -21,6 +24,22 @@ const routes = [
         path: '/register',
         name: 'Register',
         component: SignupForm,
+    },
+    {
+        //if there is JWT exists give access to page.
+        //need to decode token on client side to confirm dont know that part yet.
+        path: '/products',
+        name: 'Products',
+        component: Products,
+        beforeEnter: (to, from, next) => {
+            const token = VueCookies.isKey('token')
+            if (token == true) {
+                next()
+            } else {
+                next('/register')
+                console.log('token is not available')
+            }
+        },
     },
 ]
 
