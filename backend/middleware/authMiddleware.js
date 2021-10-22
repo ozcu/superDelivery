53cec343b3f4a74e3a4f242e28b11dd0a6068bda
Ugurhan Buyukozcu <ugurhan.buyukozcu@gmail.com>
2021-10-22 21:57:21 +0300

    to be removed

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -1,14 +1,15 @@
 const jwt = require('jsonwebtoken')
 
 const requireAuth = (req, res, next) => {
-    const token = req.cookies.jwt
+    // const token = req.body.token
+    const token = req.body
 
     //check JWT exists and verified
     if (token) {
         jwt.verify(token, 'secretid', (err, decodedToken) => {
             if (err) {
                 console.log(err.message)
-                res.redirect('/login')
+                res.redirect('http://localhost:8080/login')
             } else {
                 console.log(decodedToken)
                 next()
