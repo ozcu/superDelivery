5443b9614b7eabfc803743daad54f90e8d530e9d
Ugurhan Buyukozcu <ugurhan.buyukozcu@gmail.com>
2021-10-25 00:37:38 +0300

    add global vue cookie config

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,11 +3,14 @@ import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import store from './store'
+import VueCookies from 'vue-cookies'
 
 Vue.config.productionTip = false
+VueCookies.config('3d', '', '', true,'Lax')
 
 new Vue({
     router,
     store,
+    VueCookies,
     render: (h) => h(App),
 }).$mount('#app')
