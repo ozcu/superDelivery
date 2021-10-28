e02cbf489c2cd4bf78ffa71ed9dd3830a5837679
Ugurhan Buyukozcu <ugurhan.buyukozcu@gmail.com>
2021-10-28 15:01:14 +0300

    add fetchproducts method

--- frontend/src/components/Products.vue
+++ frontend/src/components/Products.vue
@@ -1,13 +1,37 @@
-
-
 <script>
-export default {}
+import { mapActions } from 'vuex'
+export default {
+    data() {
+        return {
+            fields: ['name', 'category', 'price'],
+            products: [],
+        }
+    },
+    async mounted() {
+        this.products = await this.fetchProducts()
+        console.log(this.products)
+    },
+
+    methods: {
+        ...mapActions(['fetchProducts']),
+    },
+}
 </script>
 
 <template>
     <div>
-        <h1>Products</h1>
+        <h1 style="margin-bottom: 50px">Products</h1>
+
+        <div>
+            <b-table
+                class="mx-auto border border-secondary"
+                style="width: 800px"
+                striped
+                hover
+                :items="products"
+                :fields="fields"
+            ></b-table>
+        </div>
     </div>
 </template>
-
 <style></style>
