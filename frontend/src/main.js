4c99836aafa1cfac75a65b328f2c5910dc897fc7
Ugurhan Buyukozcu <ugurhan.buyukozcu@gmail.com>
2021-10-28 15:03:39 +0300

    add bootstrap

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -6,11 +6,15 @@ import store from './store'
 import VueCookies from 'vue-cookies'
 import axios from 'axios'
 
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+
 Vue.config.productionTip = false
 VueCookies.config('3d', '', '', true, 'Lax')
 axios.defaults.baseURL = 'http://localhost:3000'
-
-
+Vue.use(BootstrapVue)
+Vue.use(IconsPlugin)
 
 new Vue({
     router,
