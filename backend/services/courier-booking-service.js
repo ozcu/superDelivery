4d6f6010073e88c2841938789724f9a0043a6368
Ugurhan Buyukozcu <ugurhan.buyukozcu@gmail.com>
2021-10-29 22:28:36 +0300

    Move depotService methods to depotService

--- backend/services/courier-booking-service.js
+++ backend/services/courier-booking-service.js
@@ -3,27 +3,9 @@ const courierService = require('./courier-service')
 const depotService = require('./depot-service')
 
 class CourierBookingService extends BaseService {
-    async addCourierToDepot(courierId, depotId) {
-        const courier = await courierService.find(courierId)
-        const depot = await depotService.find(depotId) //find a gerek yok add eklenebilir
-
-        await depot.personnel.push(courier)
-        await depotService.update(depotId, depot)
-
-        return depot, courier //obje alıyor
-    }
-
-    async removeCourierFromDepot(courierId, depotId) {
-        const courier = await courierService.find(courierId)
-        const depot = await depotService.find(depotId)
-
-        const index = await depot.personnel.indexOf(courier)
-        await depot.personnel.splice(index, 1)
-
-        await depotService.update(depotId, depot)
-
-        return depot, courier
-    }
+    //user coordinates will be checked with closest depot coordinates
+    //depot will assign available courier
+    //user will book courier after order finalized.
 }
 
 module.exports = new CourierBookingService()
