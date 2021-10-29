e0dd1449f6e2defcd6abbb8a7d2aea3e005ab2a3
Ugurhan Buyukozcu <ugurhan.buyukozcu@gmail.com>
2021-10-29 22:27:41 +0300

    Add new method

--- backend/services/product-service.js
+++ backend/services/product-service.js
@@ -2,12 +2,15 @@ const BaseService = require('./base-service')
 const Product = require('../models/product')
 
 class ProductService extends BaseService {
-  async findByName(name) {
-    return this.findBy('name', name)
-  }
-  async findById(id) {
-    return this.findBy('_id', id)
-  }
+    async findByName(name) {
+        return this.findBy('name', name)
+    }
+    async findById(id) {
+        return this.findBy('_id', id)
+    }
+    async find(id) {
+        return this.findById(id)
+    }
 }
 
 module.exports = new ProductService(Product)
