10f757f111d45600b6356d8dde6d7b402daa2650
Ugurhan Buyukozcu <ugurhan.buyukozcu@gmail.com>
2021-10-29 12:24:26 +0300

    add card view for products

--- frontend/src/components/Products.vue
+++ frontend/src/components/Products.vue
@@ -9,7 +9,7 @@ export default {
     },
     async mounted() {
         this.products = await this.fetchProducts()
-        console.log(this.products)
+
     },
 
     methods: {
@@ -21,17 +21,50 @@ export default {
 <template>
     <div>
         <h1 style="margin-bottom: 50px">Products</h1>
+        <div class="container-lg">
+            <div class="row">
+                <!-- <div class="col-9">
+                    <div>
+                        <b-table
+                            class="mx-auto border border-secondary"
+                            style="width: 800px"
+                            striped
+                            hover
+                            :items="products"
+                            :fields="fields"
+                        ></b-table>
+                    </div>
+                </div> -->
 
-        <div>
-            <b-table
-                class="mx-auto border border-secondary"
-                style="width: 800px"
-                striped
-                hover
-                :items="products"
-                :fields="fields"
-            ></b-table>
+                <div class="p-g bg-secondary text-light text-font-5 h2">
+                    Product List
+                </div>
+                <div
+                    class="col-2"
+                    v-for="product in products"
+                    :key="product.id"
+                >
+                <b-card>{{product.price}} TL</b-card>
+                    <b-card
+                        :title="product.name"
+                        img-src="https://picsum.photos/600/300/?image=25"
+                        img-alt="Image"
+                        img-top
+                        tag="article"
+                        style="max-width: 15rem"
+                        class="mb-2"
+                    >
+                        <b-card-text class="text">
+                            {{ product.description }}
+                        </b-card-text>
+                    </b-card>
+                </div>
+            </div>
         </div>
     </div>
 </template>
-<style></style>
+<style>
+.text {
+    font-size: 12px;
+}
+</style>
