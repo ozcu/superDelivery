e22d2bf14239ddac149bcb6297f1949ac46a2032
Ugurhan Buyukozcu <ugurhan.buyukozcu@gmail.com>
2021-10-29 22:29:41 +0300

    Fix Courier Schema

--- backend/models/courier.js
+++ backend/models/courier.js
@@ -1,11 +1,9 @@
 const mongoose = require('mongoose')
 
-const CourierSchema = new mongoose.Schema(
-  {
-    courierName: { type: String, required: true, minlength: 2 },
+const CourierSchema = new mongoose.Schema({
+    name: { type: String, required: true, minlength: 2 },
     rating: Number,
-  },
-  { timestamps: true },
-)
-//CourierSchema.plugin(require('mongoose-autopopulate'))
-module.exports = mongoose.model('Courier', CourierSchema)
+    available: Boolean,
+})
+const Courier = mongoose.model('Courier', CourierSchema)
+module.exports = Courier
