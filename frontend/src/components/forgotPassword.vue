2f68543dd2bd5a394bb7fdc5bf07e0c93e24288d
Ugurhan Buyukozcu <ugurhan.buyukozcu@gmail.com>
2021-10-30 21:10:20 +0300

    Add forgot password view

---
+++ frontend/src/components/forgotPassword.vue
@@ -0,0 +1,37 @@
+<template>
+    <form class="form" @submit.prevent="forgotPassword">
+        <h1 class="title">Forgot Password</h1>
+        <div class="form">
+            <label>Email</label>
+            <input
+                type="email"
+                v-model="$store.state.email"
+                placeholder="Email"
+            />
+        </div>
+
+        <button>Send</button>
+    </form>
+</template>
+
+<script>
+import { mapState, mapActions } from 'vuex'
+export default {
+    name: 'forgotPassword',
+    computed: {
+        ...mapState(['email']),
+    },
+    methods: {
+        ...mapActions(['forgotPassword']),
+    },
+}
+</script>
+
+<style>
+.title {
+    text-align: center;
+    color: #0b6dff;
+    border: none;
+    padding: 0;
+}
+</style>
