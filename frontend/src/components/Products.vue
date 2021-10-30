ce515ae4b97906164064dd56aaea5ade5b2eb163
Ugurhan Buyukozcu <ugurhan.buyukozcu@gmail.com>
2021-10-30 21:09:45 +0300

    Add product search

--- frontend/src/components/Products.vue
+++ frontend/src/components/Products.vue
@@ -3,48 +3,52 @@ import { mapActions } from 'vuex'
 export default {
     data() {
         return {
-            fields: ['name', 'category', 'price'],
+            query: '',
             products: [],
+            matchedProducts: [],
+            isSearched: false,
         }
     },
     async mounted() {
         this.products = await this.fetchProducts()
-
     },
 
     methods: {
         ...mapActions(['fetchProducts']),
+        findProducts() {
+            return this.products.filter((product) => {
+                const regex = new RegExp(this.query, 'gi')
+                return product.name.match(regex) //|| product.category.match(regex) bu calısmadı sonra bak
+            })
+        },
+        displayProducts() {
+            const products = this.findProducts()
+            this.matchedProducts = products
+            this.isSearched = true
+            return this.matchedProducts
+        },
     },
 }
 </script>
 
 <template>
     <div>
-        <h1 style="margin-bottom: 50px">Products</h1>
+        <h1>Products</h1>
+        <div class="searchbar">
+            <b-form-input
+                v-model="query"
+                @keyup="displayProducts"
+                placeholder="Search Product"
+            ></b-form-input>
+        </div>
         <div class="container-lg">
-            <div class="row">
-                <!-- <div class="col-9">
-                    <div>
-                        <b-table
-                            class="mx-auto border border-secondary"
-                            style="width: 800px"
-                            striped
-                            hover
-                            :items="products"
-                            :fields="fields"
-                        ></b-table>
-                    </div>
-                </div> -->
-
-                <div class="p-g bg-secondary text-light text-font-5 h2">
-                    Product List
-                </div>
+            <div v-if="!isSearched" class="row">
                 <div
                     class="col-2"
                     v-for="product in products"
                     :key="product.id"
                 >
-                <b-card>{{product.price}} TL</b-card>
+                    <b-card>{{ product.price }} TL</b-card>
                     <b-card
                         :title="product.name"
                         img-src="https://picsum.photos/600/300/?image=25"
@@ -54,9 +58,33 @@ export default {
                         style="max-width: 15rem"
                         class="mb-2"
                     >
-                        <b-card-text class="text">
+                        <b-card-text class="description">
                             {{ product.description }}
                         </b-card-text>
+                        <button class="bg-secondary text-light p-1">+</button>
+                    </b-card>
+                </div>
+            </div>
+            <div v-else class="row">
+                <div
+                    class="col-2"
+                    v-for="matchedProduct in matchedProducts"
+                    :key="matchedProduct.id"
+                >
+                    <b-card>{{ matchedProduct.price }} TL</b-card>
+                    <b-card
+                        :title="matchedProduct.name"
+                        img-src="https://picsum.photos/600/300/?image=25"
+                        img-alt="Image"
+                        img-top
+                        tag="article"
+                        style="max-width: 15rem"
+                        class="mb-2"
+                    >
+                        <b-card-text class="description">
+                            {{ matchedProduct.description }}
+                        </b-card-text>
+                        <button class="bg-secondary text-light p-1">+</button>
                     </b-card>
                 </div>
             </div>
@@ -64,7 +92,14 @@ export default {
     </div>
 </template>
 <style>
-.text {
+.description {
     font-size: 12px;
+}
+.searchbar {
+    margin-bottom: 10px;
+    display: block;
+    margin-left: auto;
+    margin-right: auto;
+    width: 500px;
 }
 </style>
