e5fe6da49c8629c0273b1a60bf46df05a1afab7b
Ugurhan Buyukozcu <ugurhan.buyukozcu@gmail.com>
2021-10-30 21:10:42 +0300

    Add findbyemail method

--- backend/services/user-service.js
+++ backend/services/user-service.js
@@ -2,12 +2,15 @@ const BaseService = require('./base-service')
 const User = require('../models/user')
 
 class UserService extends BaseService {
-  async findByName(name) {
-    return this.findBy('name', name)
-  }
-  async findById(id) {
-    return this.findBy('_id', id)
-  }
+    async findByName(name) {
+        return this.findBy('name', name)
+    }
+    async findById(id) {
+        return this.findBy('_id', id)
+    }
+    async findByEmail(email) {
+        return this.findBy('email', email)
+    }
 }
 
 module.exports = new UserService(User)
