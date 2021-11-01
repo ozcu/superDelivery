1b79ece4b42106a2d8984614f55ba720be7cff68
Ugurhan Buyukozcu <ugurhan.buyukozcu@gmail.com>
2021-11-01 12:47:00 +0300

    Add todo comment

--- backend/routes/baskets.js
+++ backend/routes/baskets.js
@@ -1,37 +1,37 @@
 const express = require('express')
 const router = express.Router()
 const { basketService } = require('../services')
-
+//add remove olmamalı başlarında değiştir
 router.post('/add/:userId/:productId', async (req, res) => {
-  try {
-    const { userId } = req.params
-    const { productId } = req.params
-    await basketService.addProductToBasket(userId, productId)
-    res.send('OK')
-  } catch (e) {
-    throw new Error('Product not added to basket!')
-  }
+    try {
+        const { userId } = req.params
+        const { productId } = req.params
+        await basketService.addProductToBasket(userId, productId)
+        res.send('OK')
+    } catch (e) {
+        throw new Error('Product not added to basket!')
+    }
 })
 
 router.post('/remove/:userId/:productId', async (req, res) => {
-  try {
-    const { userId } = req.params
-    const { productId } = req.params
-    await basketService.removeProductFromBasket(userId, productId)
-    res.send('OK')
-  } catch (e) {
-    throw new Error('Product not removed from basket!')
-  }
+    try {
+        const { userId } = req.params
+        const { productId } = req.params
+        await basketService.removeProductFromBasket(userId, productId)
+        res.send('OK')
+    } catch (e) {
+        throw new Error('Product not removed from basket!')
+    }
 })
 
 router.post('/emptyBasket/:userId', async (req, res) => {
-  try {
-    const { userId } = req.params
-    await basketService.emptyUserBasket(userId)
-    res.send('OK')
-  } catch (e) {
-    throw new Error('Cannot empty the basket!')
-  }
+    try {
+        const { userId } = req.params
+        await basketService.emptyUserBasket(userId)
+        res.send('OK')
+    } catch (e) {
+        throw new Error('Cannot empty the basket!')
+    }
 })
 
 module.exports = router
