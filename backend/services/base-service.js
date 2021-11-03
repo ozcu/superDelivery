6766c5fbe85a2d77352e9925761ce447e1d79684
Ugurhan Buyukozcu <ugurhan.buyukozcu@gmail.com>
2021-11-03 17:33:47 +0300

    Add try catch

--- backend/services/base-service.js
+++ backend/services/base-service.js
@@ -1,41 +1,43 @@
 class BaseService {
-  constructor(model) {
-    this.model = model
-  }
+    constructor(model) {
+        this.model = model
+    }
 
-  save(objects) {
- 
-    return this.model.insertMany(objects)
-  }
+    save(objects) {
+        return this.model.insertMany(objects)
+    }
 
-  load() {
- 
-    return this.model.find()
-  }
+    load() {
+        return this.model.find()
+    }
 
-  async insert(object) {
-    return await this.model.create(object)
-  }
+    async insert(object) {
+        try {
+            return await this.model.create(object)
+        } catch (err) {
+            console.log(err)
+        }
+    }
 
-  async removeBy(property, value) {
-    return this.model.deleteOne({ [property]: value })
-  }
+    async removeBy(property, value) {
+        return this.model.deleteOne({ [property]: value })
+    }
 
-  async update(id, object) {
-    return this.model.findByIdAndUpdate(id, object)
-  }
+    async update(id, object) {
+        return this.model.findByIdAndUpdate(id, object)
+    }
 
-  async find(id) {
-    return this.model.findById(id)
-  }
+    async find(id) {
+        return this.model.findById(id)
+    }
 
-  async query(obj) {
-    return this.model.find(obj)
-  }
+    async query(obj) {
+        return this.model.find(obj)
+    }
 
-  async findBy(property, value) {
-    return this.model.find({ [property]: value })
-  }
+    async findBy(property, value) {
+        return this.model.find({ [property]: value })
+    }
 }
 
 module.exports = BaseService
