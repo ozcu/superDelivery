faa86c800381722c2f3d526a1a910ca1e4ab2c20
Ugurhan Buyukozcu <ugurhan.buyukozcu@gmail.com>
2021-11-03 17:32:26 +0300

    Fix removeProduct method

--- backend/services/basket-service.js
+++ backend/services/basket-service.js
@@ -7,37 +7,52 @@ class BasketService extends BaseService {
         return this.findBy('_id', id)
     }
     emptyBasket = async (basketId) => {
-        const basket = await this.find(basketId)
-        basket.products = []
-        basket.basketTotal = 0
-        await this.model.findByIdAndUpdate(basketId, basket)
-        return basket
+        try {
+            const basket = await this.find(basketId)
+            basket.products = []
+            basket.basketTotal = 0
+            await this.model.findByIdAndUpdate(basketId, basket)
+            return basket
+        } catch (err) {
+            console.log(err)
+        }
     }
     addProductToBasket = async (basketId, productId) => {
-        const basket = await this.find(basketId)
-        const [product] = await productService.find(productId) //depot-service doesnt need this why??
+        try {
+            const basket = await this.find(basketId)
+            const [product] = await productService.find(productId) //depot-service doesnt need this as array why??
 
-        await basket.products.push(product)
-        basket.basketTotal += product.price
-        await this.model.findByIdAndUpdate(basketId, basket)
+            await basket.products.push(product)
+            basket.basketTotal += product.price
+            await this.model.findByIdAndUpdate(basketId, basket)
 
-        return { basket, product }
+            return { basket, product }
+        } catch (err) {
+            console.log(err)
+        }
     }
     removeProductFromBasket = async (basketId, productId) => {
-        const basket = await this.find(basketId)
-        const [product] = await productService.find(productId)
+        try {
+            const basket = await this.find(basketId)
+            const [product] = await productService.find(productId)
+            const index = await basket.products.findIndex(
+                (x) => x.id == `${product.id}`,
+            )
 
-        const index = await basket.products.indexOf(product)
-        await basket.products.splice(index, 1)
-        basket.basketTotal -= product.price
-
-        if (basket.basketTotal <= 0) {
-            basket.basketTotal = 0
+            if (index >= 0) {
+                await basket.products.splice(index, 1)
+                basket.basketTotal -= product.price
+                if (basket.basketTotal <= 0) {
+                    basket.basketTotal = 0
+                }
+                await this.model.findByIdAndUpdate(basketId, basket)
+                return { basket, product }
+            } else {
+                return
+            }
+        } catch (err) {
+            console.log(err)
         }
-
-        await this.model.findByIdAndUpdate(basketId, basket)
-
-        return { basket, product }
     }
 }
 
